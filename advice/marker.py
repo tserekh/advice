1398cf0846e4ea1bd816603193895bcf00177a57
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-07 20:23:47 +0300

    demo

--- advice/marker.py
+++ advice/marker.py
@@ -1,11 +1,65 @@
 import pandas as pd
-
-def posovet(message):
-    return int('посовет' in message)
-
-def add_mark(df: pd.DataFrame, target: str, condition):
+import io
+from typing import List
+import numpy as np
+def add_question_mark(df: pd.DataFrame)-> pd.DataFrame:
+    target = "question"
     target_list = []
-    for message in df['message'].values:
-        condition(message)
+    for message in df["message"].values:
+        target_list.append(int("посовет" in message))
     df[target] = target_list
     return df
+
+def get_reply_mapping(df: pd.DataFrame) -> pd.DataFrame:
+    """ "message", "question", "go_to_message_id", "message_id"]] """
+    df_question = (
+        df[df["question"]==1]
+        .rename(columns={"message": "question_message", "message_id": "question_message_id"})
+        [["question_message", "question_message_id"]]
+    )
+    df_reply = (
+        df[df["go_to_message_id"].notnull()]
+        .rename(
+            columns={
+                "message": "reply_message",
+                "message_id": "reply_message_id",
+                "go_to_message_id": "question_message_id"
+            }
+        )[["reply_message", "reply_message_id", "question_message_id"]]
+    )
+    return pd.merge(df_question, df_reply, on="question_message_id")
+
+class Similarity:
+    pass
+
+class FastText(Similarity):
+    def load_vectors(self, vectors_path: str, use_tokens: List[str]):
+        fin = io.open(vectors_path, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        # n, d = map(int, fin.readline().split())
+        data = {}
+        for line in fin:
+            tokens = line.rstrip().split(' ')
+            if tokens[0] in use_tokens:
+                data[tokens[0]] = list(map(float, tokens[1:]))
+        self.embeddings = data
+
+    def __init__(self, vectors_name: str,  use_tokens: List[str]):
+        self.vectors_name = vectors_name
+        self.use_tokens = use_tokens
+        self.load_vectors(self.vectors_name, self.use_tokens)
+
+    def tokenizer(self):
+        pass
+
+    def cosin(self, tokens1: List[str], tokens2: List[str]):
+        question_embedding = np.zeros(300)
+        message_embedding = np.zeros(300)
+        for token in tokens1:
+            if token in self.embeddings:
+                question_embedding += np.array(self.embeddings[token])
+        for token in tokens2:
+            if token in self.embeddings:
+                message_embedding += np.array(self.embeddings[token])
+        cos_sim = np.dot(question_embedding, message_embedding) / (
+                    np.linalg.norm(question_embedding) * np.linalg.norm(message_embedding))
+        return cos_sim
--- advice/reader.py
+++ advice/reader.py
@@ -4,42 +4,41 @@ from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
 import pandas as pd
 from tqdm import tqdm
+from typing import Dict, Iterable
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.feature_extraction.text import CountVectorizer
-
+import config
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import roc_auc_score
 
 
-def get_message(mes):
+def get_message(message: str)->Dict:
     dic = {}
-    dic['message'] = mes.text.split('\n\n')[-2]
-    dic['message_id'] = mes.get('id').replace('message', '')
-    dic['from_name'] = mes.find(attrs={'class': 'from_name'})
-    reply = mes.find(attrs={'class': 'reply_to details'})
-
-    # print(reply)
+    dic['message'] = message.text.split('\n\n')[-2]
+    dic['message_id'] = message.get('id').replace('message', '')
+    dic['from_name'] = message.find(attrs={'class': 'from_name'})
+    reply = message.find(attrs={'class': 'reply_to details'})
     if reply:
         dic['go_to_message_id'] = reply.find('a').get('href').replace('#go_to_message', '')
     return dic
 
 # папки с ChatExport
 sub_list = []
-folders = glob.glob("C:/Users/artem/Downloads/Telegram Desktop/ChatExport/ChatExport*")
-files = []
-for folder in folders:
-    files = glob.glob(f"{folder}/*.html")
-    messages_all = []
-    for file in tqdm(files):
-        with open(file, encoding='utf-8') as f:
-            soup = BeautifulSoup(f.read(), 'html.parser')
-            messages = soup.body.find_all(attrs = {'class':'message'})#.text#.split('\n\n')[-2].lower()
-        messages_all+=messages
-    chat_name = soup.find(attrs = {'class', "text bold"}).text
-    df_sub = pd.DataFrame(list(map(get_message, messages_all)))
-    df_sub["chat_name"] = chat_name
-    sub_list.append(df_sub)
-df = pd.concat(sub_list)
+def read_chats_data() -> Iterable[pd.DataFrame]:
+    folders = glob.glob(config.chats_path)
+    for folder in folders:
+        files = glob.glob(f"{folder}/*.html")
+        messages = []
+        for file in tqdm(files):
+            with open(file, encoding='utf-8') as f:
+                soup = BeautifulSoup(f.read(), 'html.parser')
+                messages_part = soup.body.find_all(attrs = {'class':'message'})
+            messages+=messages_part
+        chat_name = soup.find(attrs = {'class', "text bold"}).text
+        df = pd.DataFrame(list(map(get_message, messages)))
+        df["chat_name"] = chat_name
+        yield df
