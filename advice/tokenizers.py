1c510a296f5863106aebbe838f242aa6bb4cdbff
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-10 18:19:08 +0300

    backup

--- advice/tokenizers.py
+++ advice/tokenizers.py
@@ -2,12 +2,5 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from typing import List
 
 def tokenize(text: str) -> List[str]:
-    stop_words = ['за', 'там', 'так', 'то', 'все', 'если', '-',
-                  ':', 'но', 'как', 'по', 'у', 'есть', 'это', 'я', 'с', 'а', 'не', ')', 'и', '.', 'в', ',']
     tokens = word_tokenize(text.lower(), language="russian")
-    tokens = list(filter(lambda x: 'посовет' not in x, tokens))
-    tokens = list(filter(lambda x: x not in stop_words, tokens))
-
     return list(set(tokens))
-
-def mapping_tokenizer()
--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from advice.reader import read_chats_data
-from advice.marker import add_question_mark, get_reply_mapping
-import pandas as pd
-from nltk.tokenize import sent_tokenize, word_tokenize
-from advice.marker import FastText
-import telebot
+import glob
 import os
 from logging import getLogger
-import pickle
+
+import pandas as pd
+import telebot
+from nltk.tokenize import word_tokenize
+
+import config
+from advice.marker import FastText
+from advice.marker import add_question_mark, get_reply_mapping
+from advice.marker import single_message_mark
 
 logger = getLogger()
 question_reply_path = "data/question_reply.csv"
@@ -18,10 +21,11 @@ if os.path.exists(question_reply_path):
     question_reply = pd.read_csv(question_reply_path)
 else:
     question_reply = pd.DataFrame()
-    chats = read_chats_data()
-    for chat in chats:
-        chat_w_mark = add_question_mark(chat)
-        question_reply = question_reply.append(get_reply_mapping(chat_w_mark))
+    folders = glob.glob(config.chats_path)
+    for path in glob.glob("data/messages/*"):
+        chat = pd.read_csv(path, sep='\t', encoding='utf-8')
+        chat = add_question_mark(chat)
+        question_reply = question_reply.append(get_reply_mapping(chat))
     questions_w_replies = question_reply[['question_message', 'question_message_id']].drop_duplicates()
     question_reply["question_tokens"] = question_reply['question_message'].apply(tokenize)
     question_reply.to_csv(question_reply_path, index=False)
@@ -31,20 +35,14 @@ for question_tokens in question_reply.drop_duplicates('question_message')['quest
 
 vc = pd.Series(all_tokens).value_counts()
 use_tokens = set(vc.iloc[50:].index)
-# if os.path.exists("data/fast_text.pkl"):
-#     fast_text = pickle.load("data/fast_text.pkl")
-# else:
-#
-#     pickle.dump(fast_text, "data/fast_text.pkl")
+
 fast_text = FastText("D:/Downloads/cc.ru.300.vec/cc.ru.300.vec", use_tokens)
-
-
 with open("C:/Users/artem/Documents/sochi.txt") as f:
     TOKEN = f.read()
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: "посовет" in message.text.lower())
+@bot.message_handler(func=single_message_mark)
 def handle_message(message):
     tokens = tokenize(message.text)
     question_reply['cosin'] = question_reply['question_tokens'].apply(lambda tokens2: fast_text.cosin(tokens, tokens2))
