cb38fcaf32e16cd251cad62a02f2afbd79c34ff6
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-11 12:45:17 +0300

    change paths

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-chats_path = "C:/Users/artem/Downloads/Telegram Desktop/ChatExport/ChatExport*"
+chats_path = "data/ChatExport*"
+vectors_path = "data/cc.ru.300.vec/cc.ru.300.vec"
+question_reply_path = "data/question_reply.csv"
+messages_path = "data/messages/*"
+token_path = "credentials/sochi.txt"
 max_questions_tokens = 35
 min_question_score = 100.0
