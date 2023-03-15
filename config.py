291e604c1af54ceea37717762bb7e61668b2ab31
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-15 15:47:03 +0300

    change paths

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 chats_path = "data/ChatExport*"
 vectors_path = "data/cc.ru.300.vec"
-question_reply_path = "data/question_reply.csv"
+question_reply_path = "data/question_reply.tsv"
 messages_path = "data/messages/*"
 token_path = "credentials/sochi.txt"
 max_questions_tokens = 35
