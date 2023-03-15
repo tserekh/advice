c041ef27a10ed3750b0bc6dc941b806646a8b4a5
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-15 15:49:00 +0300

    change paths

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ else:
         question_reply = question_reply.append(get_reply_mapping(chat))
 
 question_reply["question_tokens"] = question_reply['question_message'].apply(tokenize)
-question_reply.drop_duplicates().to_csv(config.question_reply_path, index=False, encoding='utf-8', sep='\t')
+(
+    question_reply
+    .drop_duplicates(["question_message", "question_message_id"])
+    .to_csv(config.question_reply_path, index=False, encoding='utf-8', sep='\t')
+)
 
 all_tokens = []
 for question_tokens in question_reply.drop_duplicates('question_message')['question_tokens'].values:
