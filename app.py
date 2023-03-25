33df9dddc53cb124c790c440d6928d5746b1689f
Artem Tserekh <artem.tserekh@x5.ru>
2023-03-25 17:46:44 +0300

    fix logging

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        logging.FileHandler(config.log_path),
+        logging.FileHandler(config.log_path, mode="w+", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
